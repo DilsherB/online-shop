ecb2ad483421a61187de37a40ad543114be7c867
Dilsher <dilsherb@gmail.com>
2023-12-20 13:59:11 +0300

    redux store set up and api slice written.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
@@ -14,13 +12,15 @@ import ProductScreen from "./screens/ProductScreen";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} >
-          <Route path="/" element={<HomeScreen />} />
-        </Route>
-        <Route path="/product/:id" element={<ProductScreen />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/" element={<HomeScreen />} />
+          </Route>
+          <Route path="/product/:id" element={<ProductScreen />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
