df638c103a04b1af8560414cd6e5d53a0703cb1a
Dilsher <dilsherb@gmail.com>
2023-12-20 09:17:29 +0300

    custom error message created.

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
-
+import productRoutes from './routes/products.routes.js'
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const PORT = process.env.PORT || 5000;
 
 connectDB();
@@ -14,14 +14,10 @@ app.get("/", (req, res) => {
   res.send("API call");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
