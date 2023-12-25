5c6a07ef810e48173261a39b99be5e0967a07ec6
Dilsher <dilsherb@gmail.com>
2023-12-25 10:48:20 +0300

    authUser logic to check email & password is written. middleware is server.js introduced to get json data from authUser. matchPassword metthod written in user.model.js to check password.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,20 @@ import User from "../models/user.model.js";
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  res.send("Login user");
+  // res.send(req.body);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc    Logout user & clear cookie
