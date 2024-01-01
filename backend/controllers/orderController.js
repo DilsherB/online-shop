e61949b1810908a6240db164d2fec82d136ba253
Dilsher <dilsherb@gmail.com>
2024-01-01 16:20:15 +0300

    order routes corrected and tested.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,46 +1,46 @@
-import asycHandler from "express-async-handler";
-import Order from "../models/orderModel.js";
+import asycHandler from "../middleware/asyncHandler.js";
+import Order from "../models/order.model.js";
 
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
 const addOrderItems = asycHandler(async (req, res) => {
-  res.response("add order items");
+  res.send("add order items");
 });
 
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asycHandler(async (req, res) => {
-  res.response("get my orders");
+  res.send("get my orders");
 });
 
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asycHandler(async (req, res) => {
-  res.response("get order by id");
+  res.send("get order by id");
 });
 
 // @desc    Update order to paid
 // @route   PUT /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asycHandler(async (req, res) => {
-  res.response("update order to paid");
+  res.send("update order to paid");
 });
 
 // @desc    Update order to delivered
 // @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asycHandler(async (req, res) => {
-  res.response("update order to delivered");
+  res.send("update order to delivered");
 });
 
 // @desc    Get all orders
 // @route   GET /api/orders
 // @access  Private/Admin
 const getOrders = asycHandler(async (req, res) => {
-  res.response("get all orders");
+  res.send("get all orders");
 });
 
 export {
