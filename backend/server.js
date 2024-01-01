d76dc6229e379675c3af2473e6eefcb6c1df6267
Dilsher <dilsherb@gmail.com>
2024-01-01 15:58:00 +0300

    middleware created to use orterRoutes in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import productRoutes from './routes/products.routes.js'
 import userRoutes from './routes/users.routes.js'
+import orderRoutes from './routes/orders.routes.js'
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const PORT = process.env.PORT || 5000;
 
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
